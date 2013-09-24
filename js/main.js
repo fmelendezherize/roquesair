3468c058d4ac52d9b5993ebc7f8fa7964474b052
fmelendezherize <fmelendezherize@gmail.com>
2013-09-24 18:07:11 -0430

    commit 3

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
 $(function () {
     $('.banner').unslider({
-        speed: 500,               //  The speed to animate each slide (in milliseconds)
-        delay: 3000,              //  The delay between slide animations (in milliseconds)
+        speed: 1000,               //  The speed to animate each slide (in milliseconds)
+        delay: 6000,              //  The delay between slide animations (in milliseconds)
         complete: function () { },  //  A function that gets called after every slide animation
         keys: true,               //  Enable keyboard (left, right) arrow shortcuts
         dots: true,               //  Display dot navigation
         fluid: false              //  Support responsive design. May break non-responsive designs
     });
 });
+
+var idElementoActivo = "";
+function onClickRadio(val) {
+    if (val.id != idElementoActivo) {
+        idElementoActivo = val.id;
+    }
+    else {
+        val.checked = false;
+        idElementoActivo = "";
+    }
+};
