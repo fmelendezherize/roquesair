43d25552ad445a22a05e3e53a21a1a553392225d
fmelendezherize <fmelendezherize@gmail.com>
2013-10-05 15:03:49 -0430

    commit 05102013

--- js/main.js
+++ js/main.js
@@ -9,6 +9,17 @@ $(function () {
     });
 });
 
+$(function () {
+    $('.dataaeronave-banner').unslider({
+        speed: 1000,               //  The speed to animate each slide (in milliseconds)
+        delay: 6000,              //  The delay between slide animations (in milliseconds)
+        complete: function () { },  //  A function that gets called after every slide animation
+        keys: true,               //  Enable keyboard (left, right) arrow shortcuts
+        dots: true,               //  Display dot navigation
+        fluid: false              //  Support responsive design. May break non-responsive designs
+    });
+});
+
 var idElementoActivo = "";
 function onClickRadio(val) {
     if (val.id != idElementoActivo) {
