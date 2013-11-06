5e6ea2fceb43cb25210b443b3eec798cddc9a9db
fmelendezherize <fmelendezherize@gmail.com>
2013-11-06 12:43:30 -0430

    commit 06112013

--- js/main.js
+++ js/main.js
@@ -1,35 +1,4 @@
-function activarDataAeronaveBanner() {
-    var slidey = $('.dataaeronave-banner').unslider({
-        speed: 1000,               //  The speed to animate each slide (in milliseconds)
-        delay: 6000,              //  The delay between slide animations (in milliseconds)
-        complete: function () { },  //  A function that gets called after every slide animation
-        keys: true,               //  Enable keyboard (left, right) arrow shortcuts
-        dots: true,               //  Display dot navigation
-        fluid: false              //  Support responsive design. May break non-responsive designs
-    });
-}
-
-function activarBanner() {
-    $('.banner').unslider({
-        speed: 1000,               //  The speed to animate each slide (in milliseconds)
-        delay: 6000,              //  The delay between slide animations (in milliseconds)
-        complete: function () { },  //  A function that gets called after every slide animation
-        keys: true,               //  Enable keyboard (left, right) arrow shortcuts
-        dots: true,               //  Display dot navigation
-        fluid: false              //  Support responsive design. May break non-responsive designs
-    });
-}
-
-var idElementoActivo = "";
-function onClickRadio(val) {
-    if (val.id !== idElementoActivo) {
-        idElementoActivo = val.id;
-    }
-    else {
-        val.checked = false;
-        idElementoActivo = "";
-    }
-}
+/** Main Scripts **/
 
 function loadTexto(file) {
     $("#mycustomhtml").load(file, function (response, status, xhr) {
@@ -44,23 +13,11 @@ function onClickVerVideo() {
     if ($("#DataAeronaveVideo").is(":visible")) {
         $("#linkiado").text("Ver Video");
         $("#DataAeronaveVideo").hide();
-        $("#DataAeronaveBanner").show();
+        $("#carousel-datos-aeronave").show();
     } else {
         $("#linkiado").text("Ver Fotos");
         $("#DataAeronaveVideo").show();
-        $("#DataAeronaveBanner").hide();
+        $("#carousel-datos-aeronave").hide();
     }
     return false;
 }
-
-//$(document).ready(function () {
-//    $('#linkiado').click(function () {
-//        //$("#DataAeronaveBanner").hide("normal", function () {
-//        //    var slidey = $('.dataaeronave-banner').unslider(), data = slidey.data('unslider');
-//        //    $("#DataAeronaveVideo").css("display", "block")
-//        //});
-
-//        $("#DataAeronaveBanner").hide();
-//        $("#DataAeronaveVideo").show();
-//    });
-//});
